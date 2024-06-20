12222b9ec0d0b728ae03279230cbbd611a107924
WawanJanuar <wawanjanuar547@gmail.com>
2024-06-21 00:58:04 +0700

    penyelesaian halaman login, sign up dan forgot password

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    if (window.innerWidth <= 500) {
-        const elements = document.querySelectorAll(".clear");
+    const elements = document.querySelectorAll(".clear");
         elements.forEach(function(element) {
             element.remove();
         });
-    }
 });
